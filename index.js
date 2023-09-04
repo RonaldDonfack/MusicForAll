91fecd791ff68d8e89351fbcae6034baa0483bab
RonaldDonfack <donfackronald13@gmail.com>
2023-09-04 23:26:45 +0100

    remain remote entring of features

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerGroup : 2,
     loop: true,
     grabCursor : true,
-       autoplay:{delay: 4500},
+       autoplay:{delay: 4500,
+        speed : 5000,
+      },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -27,3 +29,35 @@ var swiper = new Swiper(".mySwiper", {
        
     }
   });
+
+  document.addEventListener("DOMContentLoaded",() => {
+      const body = document.querySelector("body")
+      const header = document.querySelector(".header")
+      const language = document.querySelector(".language")
+      const languageInToggle = document.querySelector(".language-in-toggle")
+      const ddlanguage = document.querySelector(".ddtl")
+      const dropdownLanguage = document.querySelector(".dropdown-language")
+      const  togglebtn = document.querySelector(".togglebtn")
+      const dropdownbtn = document.querySelector('.dropdown-button')
+
+      language.onclick = ()=>{
+        dropdownLanguage.classList.toggle("dropdown-language-open")   
+      }
+      language.onblur = () => {
+        dropdownLanguage.classList.add('dropdown-language')
+      }
+      languageInToggle.onclick = () =>{
+
+        ddlanguage.classList.toggle("ddtle") 
+      }
+      togglebtn.onclick = ()=>{
+        dropdownbtn.classList.toggle("ddbstatus") 
+        
+      }
+      body.onscroll = () => {
+        header.classList.add("header-scroll")
+      }
+      body.onclick = () => {
+        header.classList.remove("header-scrool")
+      }
+  })
